72df5144cebe32a6e44e2aea494b25d189c6f9ce
fahim-softvence <fahimsoftvence2025@gmail.com>
2025-04-03 12:51:00 +0600

    modified

--- src/components/Home/Header.jsx
+++ src/components/Home/Header.jsx
@@ -30,7 +30,7 @@ const Header = () => {
           quam et lacus netus.
         </p>
         <Link
-          to="/registration"
+          to="/auth/registration"
           className="px-5 py-2 sm:px-8 sm:py-3 rounded font-medium inline-flex hover:bg-transparent border-2 duration-500 hover:text-primaryGreen border-primaryGreen items-center gap-2 text-gray-100 bg-primaryGreen"
         >
           <FaRegUserCircle className="text-xl" />
